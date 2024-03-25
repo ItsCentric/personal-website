3f915ccf402b12f46e08ccb559b6cccab1a8ca63
Isaiah <isaiahmstockton@gmail.com>
2024-03-25 14:51:47 -0400

    resdesign navbar
    - change color scheme and fonts in tailwind config
    - remove navbar component and move to layout
    - use dark theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	import Navbar from '$lib/components/Navbar.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import '../app.pcss';
 </script>
 
-<div class="flex h-full flex-col">
-	<Navbar />
-	<span class="flex-grow">
-		<slot />
-	</span>
-</div>
+<nav class="flex items-center justify-between border-b border-b-muted px-8 py-4">
+	<div>
+		<a href="/" class="font-heading text-3xl font-bold">Isaiah.</a>
+	</div>
+	<ul class="flex items-center gap-8 text-center">
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="#about">About</a>
+		</li>
+		<li>
+			<a href="#skills">Skills</a>
+		</li>
+		<li>
+			<a href="#projects">Projects</a>
+		</li>
+	</ul>
+	<Button href="/blog">View Blog</Button>
+</nav>
+<slot />
