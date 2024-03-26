c1d7bd5b490a26de3672a44fe8c844521c85b67f
Isaiah <isaiahmstockton@gmail.com>
2024-03-25 23:23:17 -0400

    redesign hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,49 @@
 	import Carousel from '$lib/components/ui/carousel/carousel.svelte';
 </script>
 
-<main class="h-full">
-	<section class="flex h-full flex-col items-center justify-center">
-		<div class="mb-4 text-center">
-			<h1 class="mb-1 text-5xl font-bold">Hey, I'm Isaiah Stockton.</h1>
-			<h2 class="text-2xl text-slate-500">Software Engineering student, based in Florida</h2>
+<main>
+	<section class="grid h-screen grid-cols-[33%,_1fr]">
+		<div class="flex h-full flex-col">
+			<div class="flex flex-grow flex-col justify-center pl-20">
+				<div class="mb-4">
+					<h1 class="mb-2 font-heading text-7xl font-bold">Hey, I'm Isaiah Stockton</h1>
+					<p class="text-2xl text-muted">Making the world a better place, one line at a time.</p>
+				</div>
+				<div class="flex gap-4">
+					<Button href="/resume.pdf" target="_blank">View resume</Button>
+					<Button variant="link" disabled>View blog</Button>
+				</div>
+			</div>
+			<div class="flex h-1/4 items-center gap-8 bg-black px-20 py-40">
+				<h2 class="font-heading text-4xl font-semibold">Social links</h2>
+				<ul class="flex w-full flex-col divide-y divide-muted">
+					<li class="flex items-center justify-between pb-2 pl-2">
+						<p class="text-lg font-semibold">GitHub</p>
+						<Button
+							size="lg"
+							variant="link"
+							href="https://www.github.com/itscentric"
+							target="_blank">Follow</Button
+						>
+					</li>
+					<li class="flex items-center justify-between py-2 pl-2">
+						<p class="text-lg font-semibold">LinkedIn</p>
+						<Button size="lg" variant="link" disabled>Follow</Button>
+					</li>
+					<li class="flex items-center justify-between pl-2 pt-2">
+						<p class="text-lg font-semibold">Twitter/X</p>
+						<Button
+							size="lg"
+							variant="link"
+							href="https://www.twitter.com/theonlycentric"
+							target="_blank">Follow</Button
+						>
+					</li>
+				</ul>
+			</div>
 		</div>
-		<div class="flex gap-2">
-			<Button variant="outline" href="static/resume.pdf" download="Isaiah_Stockton_Resume.pdf">
-				Download Resume
-			</Button>
-			<Button href="#contact">Let's Connect</Button>
+		<div class="h-full">
+			<img src="/development.jpg" alt="Lines of code" class="row-span-2 h-full" />
 		</div>
 	</section>
 	<section class="flex bg-slate-200 py-16">
