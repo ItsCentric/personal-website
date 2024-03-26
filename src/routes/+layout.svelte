9b58f5217336f8b41960ef05ef3768ae3857f582
Isaiah <isaiahmstockton@gmail.com>
2024-03-25 23:23:39 -0400

    fix nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,26 @@
 	import '../app.pcss';
 </script>
 
-<nav class="flex items-center justify-between border-b border-b-muted px-8 py-4">
+<nav
+	class="absolute flex w-full items-center justify-between border-b border-b-muted bg-background px-20 py-4"
+>
 	<div>
 		<a href="/" class="font-heading text-3xl font-bold">Isaiah.</a>
 	</div>
 	<ul class="flex items-center gap-8 text-center">
 		<li>
-			<a href="/">Home</a>
+			<Button variant="link" href="/">Home</Button>
 		</li>
 		<li>
-			<a href="#about">About</a>
+			<Button variant="link" href="#about">About</Button>
 		</li>
 		<li>
-			<a href="#skills">Skills</a>
+			<Button variant="link" href="#skills">Skills</Button>
 		</li>
 		<li>
-			<a href="#projects">Projects</a>
+			<Button variant="link" href="#projects">Projects</Button>
 		</li>
 	</ul>
-	<Button href="/blog">View Blog</Button>
+	<Button disabled>View Blog</Button>
 </nav>
 <slot />
