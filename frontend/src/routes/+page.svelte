16c39cda368b4c4d2fedd4b014d72fc3208fc4d9
Isaiah <isaiahmstockton@gmail.com>
2025-05-09 23:24:35 -0400

    fix: song card has unconstrained width

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -83,7 +83,7 @@
 				</div>
 				<Separator class="lg:hidden" />
 				<Separator orientation="vertical" class="hidden h-1/2 lg:block" />
-				<div class="flex flex-1 items-center gap-8">
+				<div class="flex flex-1 items-center max-w-lg gap-8">
 					<h2 class="max-w-min font-heading text-2xl font-semibold lg:text-4xl">Listening to...</h2>
 					<div class="flex flex-1 items-center gap-4 rounded-lg bg-white/20 px-4 py-2 shadow-lg">
 						{#if currentSong}
