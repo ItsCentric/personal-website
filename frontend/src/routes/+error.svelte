71d724c1f29c429bde493841b3c3602ffd5cb9ad
Isaiah <isaiahmstockton@gmail.com>
2025-05-09 22:45:22 -0400

    feat: add custom error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<main class="flex relative h-screen flex-col items-center justify-center pt-[93px] px-8 overflow-hidden">
+	<p
+		class="absolute select-none text-center font-heading rotate-90 lg:rotate-0 text-[55vh] lg:text-[85vh] opacity-5 text-gradient"
+	>
+		500
+	</p>
+	<h1 class="mb-2 font-heading text-2xl lg:text-4xl">Well... Shucks...</h1>
+	<div class="text-center">
+		<p class="lg:text-xl text-muted mb-1">
+			Whatever that was wasn't supposed to happen. Sorry about that, try again later?
+		</p>
+		<p class="text-xs lg:text-sm text-muted">
+			(By later, I know you're just going to refresh the page instantly, but I mean like... later
+			later)
+		</p>
+	</div>
+</main>
+
+<style>
+	.text-gradient {
+		mask-image: linear-gradient(180deg, white, transparent);
+	}
+</style>
